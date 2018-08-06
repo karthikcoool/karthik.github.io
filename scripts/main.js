97d342ca698d3c99b22535dddee9527dd40330a7
karthikcoool <31313595+karthikcoool@users.noreply.github.com>
2018-08-06 15:25:26 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,3 @@
-﻿var myHeading = document.querySelector('h1');
-myHeading.textContent = 'KARTHIKs world!';
-var myVar = setInterval(myTimer, 10000);
-var i=0;
-var k;
-function myTimer()
-{     if( i==1)
-        {  alert("Welcome to Karthik's Kingdom");
-	} 
- if( i==2)
-        {  alert("Karthik is alone, call him ");
-	   alert(" he he he just kidding, he is busy");
-	} 
- if(i==4)
- {     alert("karthik is the Chairman of the company RPLTechno IT Services Pvt Ltd");
-  
- }
-  if(i==5)
- {       alert(" your mobile going to blast");
- }
-  if(i==6)
- { i=0;
- }
- 
-	i++;
-
- 
- 
-
-}
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+var randomMessage  ='';
+randomMessage = Math.floor( Math.random()*8);
+console.log(randomMessage);
